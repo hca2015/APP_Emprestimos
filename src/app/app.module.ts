8572aab1ab30147c9e20cf7b81980bcc8d0702be
Haryel Costa Assenção <haryel.assencao@dpworld.com>
2021-07-06 15:07:24 -0300

    autenticacao - enviar token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { AuthInterceptor } from './util';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FlexLayoutModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/util.ts
+++ src/app/util.ts
@@ -1,29 +1,56 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
 import { FormControl } from "@angular/forms";
+import { Observable } from "rxjs";
 
 export function resolveError(obj: FormControl) {
-    let erros = []
-    if (obj && obj.value && obj.errors) {
-      for (const key in obj.errors) {
-        if (Object.prototype.hasOwnProperty.call(obj.errors, key)) {
-          const element = obj.errors[key];
-          let a = ''
+  let erros = []
+  if (obj && obj.value && obj.errors) {
+    for (const key in obj.errors) {
+      if (Object.prototype.hasOwnProperty.call(obj.errors, key)) {
+        const element = obj.errors[key];
+        let a = ''
 
-          switch (key) {
-            case 'maxlength':
-              erros.push(`Não pode exceder ${element['requiredLength']} caracteres`)
-              break;
-            case 'required':
-              erros.push(`Campo obrigatorio`)
-              break;
-            case 'email':
-              erros.push(`Email inválido`)
-              break;
-            default:
-              erros.push(`erro desconhecido ${key}`)
-              break;
-          }
+        switch (key) {
+          case 'maxlength':
+            erros.push(`Não pode exceder ${element['requiredLength']} caracteres`)
+            break;
+          case 'required':
+            erros.push(`Campo obrigatorio`)
+            break;
+          case 'email':
+            erros.push(`Email inválido`)
+            break;
+          default:
+            erros.push(`erro desconhecido ${key}`)
+            break;
         }
       }
     }
-    return erros;
   }
+  return erros;
+}
+
+
+export class AuthInterceptor implements HttpInterceptor {
+  hasToken: boolean;
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    if (window.localStorage.getItem('token'))
+      this.hasToken = true;
+    else
+      this.hasToken = false;
+
+    if (this.hasToken) {
+      req = req.clone({
+        setHeaders: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${window.localStorage.getItem('token')}`,
+        },
+      });
+    }
+
+    return next.handle(req);
+  }
+}
